efd86e66b1cfd3f0f0cce0923b4671c059b35eef
bkpark <bokyoungp@naver.com>
2023-10-25 15:13:34 +0900

    servlet day5 v4.0

--- src/main/java/ch08/NewsApiService.java
+++ src/main/java/ch08/NewsApiService.java
@@ -3,11 +3,10 @@ package ch08;
 import ch07.News;
 import ch07.NewsDAO;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.sql.SQLException;
+import java.util.List;
 
 @Path("/news")
 public class NewsApiService {
@@ -26,6 +25,46 @@ public class NewsApiService {
         } catch (SQLException e) {
             e.printStackTrace();
             result = "news api : 뉴스 등록 실패";
+        }
+        return result;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<News> list(){
+        List<News> newsList = null;
+        try {
+            newsList = newsDAO.getAll();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return newsList;
+    }
+
+    @GET
+    @Path("{aid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public News getNews(@PathParam("aid") int aid){
+        News news = null;
+        try {
+            news = newsDAO.getNews(aid);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return news;
+    }
+
+    @DELETE
+    @Path("{aid}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String delNews(@PathParam("aid") int aid) {
+        String result = "";
+        try {
+            newsDAO.delNews(aid);
+            result = "news API :  뉴스 삭제 성공";
+        } catch (SQLException e) {
+            e.printStackTrace();
+            result = "news API : 뉴스 삭제 실패";
         }
         return result;
     }
